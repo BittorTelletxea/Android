953468e5ca1bec18341d805f45aa7295b9df7639
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-11 13:20:12 +0200

    Bim Calculator demo

---
+++ BIM_Calculator/app/src/main/java/com/example/bim_calculator/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.bim_calculator;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText ageInput, feetInput, inchesInput, heightInput;
+    private TextView resultInput;
+    private Button btnCalcular;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ageInput =  findViewById(R.id.age);
+        feetInput = findViewById(R.id.feet);
+        inchesInput = findViewById(R.id.inches);
+        heightInput = findViewById(R.id.heiht);
+        heightInput = findViewById(R.id.heiht);
+        resultInput = findViewById(R.id.result);
+        btnCalcular = findViewById(R.id.calcular);
+
+        btnCalcular.setOnClickListener(v -> {
+            int edad = Integer.parseInt(ageInput.getText().toString());
+            int pies = Integer.parseInt(feetInput.getText().toString());
+            int pulgadas = Integer.parseInt(inchesInput.getText().toString());
+            float pesoKg = Float.parseFloat(heightInput.getText().toString());
+
+            int totalPulgadas = (pies * 12) + pulgadas;
+            double alturaMetros = totalPulgadas * 0.0254;
+
+            double bmi = pesoKg / (alturaMetros * alturaMetros);
+
+            resultInput.setText("Tu BMI es: " + String.format("%.2f", bmi));
+
+        });
+    }
+}
