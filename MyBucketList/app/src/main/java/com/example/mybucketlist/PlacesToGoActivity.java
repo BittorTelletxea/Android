1da9cf5bb6fa9e61614fa1d3687941685518f476
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-23 13:55:28 +0200

    Bucket activities ;

---
+++ MyBucketList/app/src/main/java/com/example/mybucketlist/PlacesToGoActivity.java
@@ -0,0 +1,20 @@
+package com.example.mybucketlist;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PlacesToGoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_places_to_go);
+
+    }
+}
